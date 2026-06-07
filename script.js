e1b51e35e8059113d764d7d87f84887e3d40f10a
POUSEIRO <kaue.pouseiro.sousa@escola.pr.gov.br>
2026-06-07 13:37:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const inicio = document.getElementById("inicio");
+const criacao = document.getElementById("criacao");
+const alimentacao = document.getElementById("alimentacao")
+const desemvolvimento = document.getElementById("desemvolvimento")
+
+inicio.addEventListener("click", () => {
+    window.location.href= "index.html";
+});
+
+criacao.addEventListener("click", () => {
+    window.location.href= "criacao.html";
+});
+
+alimentacao.addEventListener("click", () => {
+    window.location.href= "alimentacao.html";
+});
+
+desemvolvimento.addEventListener("click", () => {
+    window.location.href= "desemvolvimento.html";
+});
+
+
+
+
